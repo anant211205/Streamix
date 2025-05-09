c63c88a71169cd2c57fba57343e32d294d284993
anant211205 <anant211205@gmail.com>
2025-05-10 02:20:22 +0530

    prod

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import path from "path"
+
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 import chatRouter from "./routes/chat.route.js"
@@ -13,6 +15,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT ;
 
+const __dirname = path.resolve() ;
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true //allowing frontend to send cookies
@@ -22,7 +26,14 @@ app.use(cookieParser()) ;
 
 app.use("/api/auth" , authRouter) ;
 app.use("/api/users" , userRouter) ;
-app.use("/api/chat" , chatRouter)
+app.use("/api/chat" , chatRouter);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist"))) ;
+    app.get("*" , (req,res) => {
+        res.sendFile(path.join(__dirname, "../frontend" ,"dist" ,"index.html"))
+    })
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
